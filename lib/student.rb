2571298643191b65f928e06bfe235c743d4c53ae
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-04 01:00:41 -0400

    commit tree cfa51a1fb657ef2f0030fdbf187322b07138ae73

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,33 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # attributes_hash.each do |key, value|
+    # self.send(("#{key}="), attributes_hash[value])
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
+    self.send("profile_url=", attributes_hash[:profile_url])
+    # end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
